88f3ea6b36a3568ad106748432f7b6cece097625
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-05-02 11:05:08 +1200

    finished rounds error checking and message

--- 01_start_component.py
+++ 01_start_component.py
@@ -24,37 +24,52 @@ class ChooseRounds:
         self.entry_frame.grid(row=3)
 
         self.rounds_entry = Entry(self.entry_frame, width=34)
-        self.rounds_entry.grid(row=0, column=0, padx=5, pady=5)
+        self.rounds_entry.grid(row=0, column=0, padx=5)
 
-        self.infinite_button = Button(self.entry_frame, text="∞", width=6)
+        self.infinite_button = Button(self.entry_frame, text="∞", width=6,
+                                      command=lambda: self.to_play())
         self.infinite_button.grid(row=0, column=1, padx=5)
 
-        self.error_text = Label(self.start_frame, text="")
+        self.error_text = Label(self.start_frame, fg="#9e1e1e")
 
         self.confirm_button = Button(self.start_frame, text="Confirm",
-                                     width=15)
-        self.confirm_button.grid(row=5, padx=5)
+                                     width=15, command=lambda: self.check_rounds())
+        self.confirm_button.grid(row=5, padx=5, pady=5)
 
     # function checks if number of rounds is valid then
     # sends user to play window if rounds are valid
     def check_rounds(self):
 
         num_rounds = self.rounds_entry.get()
-        if 0 < num_rounds < 100:
-            self.to_play()
-        else:
-            if num_rounds < 0:
-                error_info = "Number too low, enter above 0"
-            elif num_rounds > 100:
-                error_info = "Number too low, enter below 100"
-                # handling text?
+        has_error = False
 
+        # try / except block checks for strings, then checks
+        # inputted number is between 0 and 100
+        try:
+            num_rounds = int(num_rounds)
+            if 0 < num_rounds < 100:
+                self.to_play()
+            else:
+                if num_rounds <= 0:
+                    error_info = "Number too low, enter above 0"
+                elif num_rounds >= 100:
+                    error_info = "Number too high, enter below 100"
+                has_error = True
+
+        except ValueError:
+            error_info = "Please enter an integer between 0 and 100."
+            has_error = True
+
+        if has_error:
             # show error to user
-            self.error_text.grid()
-            self.error_text.config()
+            self.error_text.grid(row=4, padx=5)
+            self.error_text.config(text=error_info)
 
+    # function sends user to play window
+    # -- make function when play class is being developed
     def to_play(self):
         pass
+
 
 # main routine
 if __name__ == "__main__":
